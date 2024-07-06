237e958717b3cee37ca43c5327e203e6d2d52c25
Jack Kwakman <zwartejack@gmail.com>
2024-07-06 11:39:39 +0200

    Added annotations

--- .github/workflows/validate.py
+++ .github/workflows/validate.py
@@ -15,12 +15,12 @@ def validate_csv(file_path):
     
     # Check if CSV has valid structure and contains necessary columns
     if not set(columns).issubset(df.columns):
-        print("CSV structure is not valid.")
+        print("::error file=open-cookie-database.csv,line=1,col=1::CSV structure is not valid.")
         return False
 
  # Check if 'Category' column contains only valid values
     if not df['Category'].isin(valid_categories).all():
-        print("'Category' column must contain only these values: " + ', '.join(valid_categories))
+        print("::error file=open-cookie-database.csv,line=1,col=1::'Category' column must contain only these values: " + ', '.join(valid_categories))
         invalid_categories = df[~df['Category'].isin(valid_categories)]['Category']
         print("Invalid categories are:")
         print(invalid_categories)
@@ -28,7 +28,7 @@ def validate_csv(file_path):
 
     # Check if 'ID' column contains unique UUID values
     if not (df['ID'].apply(validate_uuid).all() and df['ID'].is_unique):
-        print("'ID' column must contain unique UUID values.")
+        print("::error file=open-cookie-database.csv,line=1,col=1::'ID' column must contain unique UUID values.")
         
         non_unique_ids = df[df.duplicated('ID', keep=False)][['ID', 'Cookie / Data Key name']]
         print("Non-unique IDs and corresponding cookie names are:")
@@ -37,7 +37,7 @@ def validate_csv(file_path):
 
     # Check if 'Cookie / Data Key name' column contains unique values
     if not df['Cookie / Data Key name'].is_unique:
-        print("'Cookie / Data Key name' contains none unique values. Please check for duplicates.")
+        print("::error file=open-cookie-database.csv,line=1,col=1::'Cookie / Data Key name' contains none unique values. Please check for duplicates.")
         
         non_unique = df[df.duplicated('Cookie / Data Key name')]['Cookie / Data Key name']
         print("Non-unique values are:")
